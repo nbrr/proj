a700efdff6d5fcf136897fae322dd050a5664eb5
nbrr <nbrr@protonmail.com>
2021-08-08 21:49:41 +0200

    Span a subspace from vectors.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,23 @@
 use nalgebra::{
-    allocator::Allocator, storage::Storage, DefaultAllocator, Dim, DimMin, DimSub, Matrix, OMatrix,
-    U1,
+    allocator::Allocator, storage::Storage, DefaultAllocator, Dim, DimMin, DimName, DimSub, Matrix,
+    OMatrix, U1,
 };
 
-pub struct SubSpace<D: Dim, S: Storage<f64, D, D>> {
-    pub column_space: Matrix<f64, D, D, S>,
+#[derive(Clone, Debug)]
+pub struct SubSpace<D: Dim>
+where
+    DefaultAllocator: Allocator<f64, D, D>,
+{
+    pub column_space: OMatrix<f64, D, D>,
+    pub epsilon: f64,
 }
 
-impl<D: Dim, S: Storage<f64, D, D>> SubSpace<D, S> {
+impl<D: Dim> SubSpace<D>
+where
+    DefaultAllocator: Allocator<f64, D, D>,
+{
     fn projection<N, S1>(&self, x: &Matrix<f64, D, N, S1>) -> OMatrix<f64, D, N>
     where
-        S: Clone,
         S1: Clone + Storage<f64, D, N>,
         D: DimMin<D, Output = D> + DimSub<U1>,
         N: Dim,
@@ -22,14 +29,13 @@ impl<D: Dim, S: Storage<f64, D, D>> SubSpace<D, S> {
         let column_space_t = self.column_space.transpose();
         let symmetric = (column_space_t.clone() * self.column_space.clone()).svd(true, true);
         let column_space_tx = column_space_t * x.clone();
-        let sol = symmetric.solve(&column_space_tx, 1e-10).unwrap();
+        let sol = symmetric.solve(&column_space_tx, self.epsilon).unwrap();
         let projection: OMatrix<f64, D, N> = &self.column_space * sol;
         projection
     }
 
     fn orthogonal_projection<N, S1>(&self, x: &Matrix<f64, D, N, S1>) -> OMatrix<f64, D, N>
     where
-        S: Clone,
         S1: Clone + Storage<f64, D, N>,
         D: DimMin<D, Output = D> + DimSub<U1>,
         N: Dim,
@@ -47,7 +53,6 @@ impl<D: Dim, S: Storage<f64, D, D>> SubSpace<D, S> {
         x: &Matrix<f64, D, N, S1>,
     ) -> (OMatrix<f64, D, N>, OMatrix<f64, D, N>)
     where
-        S: Clone,
         S1: Clone + Storage<f64, D, N>,
         D: DimMin<D, Output = D> + DimSub<U1>,
         N: Dim,
@@ -60,20 +65,43 @@ impl<D: Dim, S: Storage<f64, D, D>> SubSpace<D, S> {
         let orthogonal_projection = x - &projection;
         (projection, orthogonal_projection)
     }
+
+    fn span(
+        vs: &mut [Matrix<f64, D, U1, <DefaultAllocator as Allocator<f64, D, U1>>::Buffer>],
+        epsilon: f64,
+    ) -> SubSpace<D>
+    where
+        D: DimName,
+        DefaultAllocator: Allocator<f64, D> + Allocator<f64, D, D>,
+    {
+        let mut column_space = OMatrix::<f64, D, D>::from_element(0.);
+        let n_free_vectors = Matrix::orthonormalize(vs);
+        for i in 0..n_free_vectors {
+            column_space.set_column(i, &vs[i]);
+        }
+        SubSpace {
+            column_space,
+            epsilon,
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use nalgebra::{Matrix3, Matrix3x4, Vector3};
 
-    use super::*;
+    const EPSILON: f64 = 1e-10;
 
     #[test]
     fn basic_single_vector_decomposition() {
         let a = Matrix3::new(1., 0., 0., 0., 0., 0., 0., 0., 1.);
         let x = Vector3::new(3.71, 105.12, -168.7);
 
-        let sub_space = SubSpace { column_space: a };
+        let sub_space = SubSpace {
+            column_space: a,
+            epsilon: EPSILON,
+        };
         let decomposition = sub_space.orthogonal_decomposition(&x);
         let expected = (Vector3::new(3.71, 0., -168.7), Vector3::new(0., 105.12, 0.));
 
@@ -89,7 +117,10 @@ mod tests {
         let x4 = Vector3::new(3.5, 8.3, 4.4);
         let x = Matrix3x4::from_columns(&[x1, x2, x3, x4]);
 
-        let sub_space = SubSpace { column_space: a };
+        let sub_space = SubSpace {
+            column_space: a,
+            epsilon: EPSILON,
+        };
         let decomposition = sub_space.orthogonal_decomposition(&x);
         let expected = (
             Matrix3x4::from_columns(&[
@@ -107,5 +138,37 @@ mod tests {
         );
 
         assert_eq!(decomposition, expected);
+    }
+
+    #[test]
+    fn span_tests() {
+        let spanned_sub_space = SubSpace::span(
+            &mut [
+                Vector3::new(0., 0., -6.5),
+                Vector3::new(1., 2., -23.),
+                Vector3::new(1., 2., 0.),
+                Vector3::new(0., 0., 1.),
+                Vector3::new(2., 4., 0.),
+            ],
+            EPSILON,
+        );
+
+        let a = Matrix3::new(-3.798, 4., 0., 2. * -3.798, 8., 0., 0., 0., 2.);
+        let a_sub_space = SubSpace {
+            column_space: a,
+            epsilon: EPSILON,
+        };
+
+        let x1 = Vector3::new(2.3, 4.3, 7.6);
+        let x2 = Vector3::new(8.4, 5.9, 2.9);
+        let x3 = Vector3::new(9.4, 6.8, 1.2);
+        let x4 = Vector3::new(3.5, 8.3, 4.4);
+        let x = Matrix3x4::from_columns(&[x1, x2, x3, x4]);
+
+        let from_spanned = spanned_sub_space.orthogonal_decomposition(&x);
+        let from_matrix = a_sub_space.orthogonal_decomposition(&x);
+
+        assert!((&from_spanned.0 - &from_matrix.0).norm() < EPSILON);
+        assert!((&from_spanned.1 - &from_matrix.1).norm() < EPSILON);
     }
 }
