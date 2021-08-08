ddd08c43c687008a77096a1047a11b8c2f59cfad
nbrr <nbrr@protonmail.com>
2021-08-08 20:21:01 +0200

    Cleanup traits and refactor around `SubSpace` struct.

--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,80 @@
 use nalgebra::{
-    allocator::{Allocator, SameShapeAllocator},
-    constraint::{SameNumberOfColumns, SameNumberOfRows, ShapeConstraint},
-    storage::Storage,
-    Const, DefaultAllocator, Dim, DimMin, DimMinimum, DimSub, Matrix, OMatrix,
+    allocator::Allocator, storage::Storage, DefaultAllocator, Dim, DimMin, DimSub, Matrix, OMatrix,
+    U1,
 };
 
-fn orthogonal_decomposition<D: Dim, N: Dim, S1: Storage<f64, D, D>, S2: Storage<f64, D, N>>(
-    column_space: Matrix<f64, D, D, S1>,
-    x: Matrix<f64, D, N, S2>,
-) -> (
-    Matrix<f64, D, N, <DefaultAllocator as nalgebra::allocator::Allocator<f64, D, N>>::Buffer>,
-    Matrix<
-        f64,
-        <ShapeConstraint as SameNumberOfRows<D, D>>::Representative,
-        <ShapeConstraint as SameNumberOfColumns<N, N>>::Representative,
-        <DefaultAllocator as nalgebra::allocator::Allocator<
-            f64,
-            <ShapeConstraint as SameNumberOfRows<D, D>>::Representative,
-            <ShapeConstraint as SameNumberOfColumns<N, N>>::Representative,
-        >>::Buffer,
-    >,
-)
-where
-    S1: Clone,
-    S2: Clone,
-    D: DimMin<D>,
-    DefaultAllocator: Allocator<f64, D, D>
-        + Allocator<f64, D, N>
-        + Allocator<f64, DimMinimum<D, D>, N>
-        + Allocator<f64, <D as DimMin<D>>::Output>
-        + Allocator<f64, D, <D as DimMin<D>>::Output>
-        + Allocator<f64, <D as DimMin<D>>::Output, D>
-        + Allocator<f64, D>
-        + Allocator<f64, <<D as DimMin<D>>::Output as DimSub<Const<1_usize>>>::Output>
-        + SameShapeAllocator<f64, D, N, D, N>,
-    ShapeConstraint: SameNumberOfRows<D, D> + SameNumberOfColumns<N, N>,
-    <D as DimMin<D>>::Output: DimSub<Const<1_usize>>,
-{
-    let column_space_t = column_space.transpose();
-    let symmetric = (column_space_t.clone() * column_space.clone()).svd(true, true);
-    let column_space_tx = column_space_t * x.clone();
-    let sol: OMatrix<f64, D, N> = symmetric.solve(&column_space_tx, 1e-10).unwrap();
-    let column_space_component = column_space * sol;
-    let orthogonal_component = x - &column_space_component;
-    (column_space_component, orthogonal_component)
+pub struct SubSpace<D: Dim, S: Storage<f64, D, D>> {
+    pub column_space: Matrix<f64, D, D, S>,
+}
+
+impl<D: Dim, S: Storage<f64, D, D>> SubSpace<D, S> {
+    fn projection<N, S1>(&self, x: &Matrix<f64, D, N, S1>) -> OMatrix<f64, D, N>
+    where
+        S: Clone,
+        S1: Clone + Storage<f64, D, N>,
+        D: DimMin<D, Output = D> + DimSub<U1>,
+        N: Dim,
+        DefaultAllocator: Allocator<f64, D, D>
+            + Allocator<f64, D, N>
+            + Allocator<f64, D>
+            + Allocator<f64, <D as DimSub<U1>>::Output>,
+    {
+        let column_space_t = self.column_space.transpose();
+        let symmetric = (column_space_t.clone() * self.column_space.clone()).svd(true, true);
+        let column_space_tx = column_space_t * x.clone();
+        let sol = symmetric.solve(&column_space_tx, 1e-10).unwrap();
+        let projection: OMatrix<f64, D, N> = &self.column_space * sol;
+        projection
+    }
+
+    fn orthogonal_projection<N, S1>(&self, x: &Matrix<f64, D, N, S1>) -> OMatrix<f64, D, N>
+    where
+        S: Clone,
+        S1: Clone + Storage<f64, D, N>,
+        D: DimMin<D, Output = D> + DimSub<U1>,
+        N: Dim,
+        DefaultAllocator: Allocator<f64, D, D>
+            + Allocator<f64, D, N>
+            + Allocator<f64, D>
+            + Allocator<f64, <D as DimSub<U1>>::Output>,
+    {
+        let projection = self.projection(x);
+        x - projection
+    }
+
+    fn orthogonal_decomposition<N, S1>(
+        &self,
+        x: &Matrix<f64, D, N, S1>,
+    ) -> (OMatrix<f64, D, N>, OMatrix<f64, D, N>)
+    where
+        S: Clone,
+        S1: Clone + Storage<f64, D, N>,
+        D: DimMin<D, Output = D> + DimSub<U1>,
+        N: Dim,
+        DefaultAllocator: Allocator<f64, D, D>
+            + Allocator<f64, D, N>
+            + Allocator<f64, D>
+            + Allocator<f64, <D as DimSub<U1>>::Output>,
+    {
+        let projection = self.projection(x);
+        let orthogonal_projection = x - &projection;
+        (projection, orthogonal_projection)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
     use nalgebra::{Matrix3, Matrix3x4, Vector3};
+
+    use super::*;
 
     #[test]
     fn basic_single_vector_decomposition() {
         let a = Matrix3::new(1., 0., 0., 0., 0., 0., 0., 0., 1.);
         let x = Vector3::new(3.71, 105.12, -168.7);
 
-        let decomposition = orthogonal_decomposition(a, x);
+        let sub_space = SubSpace { column_space: a };
+        let decomposition = sub_space.orthogonal_decomposition(&x);
         let expected = (Vector3::new(3.71, 0., -168.7), Vector3::new(0., 105.12, 0.));
 
         assert_eq!(decomposition, expected);
@@ -71,7 +89,8 @@ mod tests {
         let x4 = Vector3::new(3.5, 8.3, 4.4);
         let x = Matrix3x4::from_columns(&[x1, x2, x3, x4]);
 
-        let decomposition = orthogonal_decomposition(a, x);
+        let sub_space = SubSpace { column_space: a };
+        let decomposition = sub_space.orthogonal_decomposition(&x);
         let expected = (
             Matrix3x4::from_columns(&[
                 Vector3::new(2.3, 0., 7.6),
